f67d4a55cd8ecf52fd3e28d2ccbb22eb410ffd7d
John Eldrin Tolentino <115896724+jeldrint@users.noreply.github.com>
2023-05-19 09:48:17 +0800

    initial DOM setup

---
+++ src/component/0_MainPage.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const MainPage = () => {
+    return(
+        <div className='container'>
+            <header>
+                <span>image1</span>
+                <span>image2</span>
+                <span>image3</span>
+            </header>
+            <main>
+    
+            </main>
+            <footer>
+    
+            </footer>
+        </div>
+  
+    )
+}
+
+export default MainPage
