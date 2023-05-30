838e49497ca5eaa62cdd4130ec2f3a231ebc3996
John Eldrin Tolentino <115896724+jeldrint@users.noreply.github.com>
2023-05-30 13:26:57 +0800

    replaced BrowserRouter with HashRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './App.css';
 import MainPage from './components/0_MainPage';
 import React from 'react';
@@ -6,9 +6,9 @@ import React from 'react';
 const App = () => {
 
   return (
-    <BrowserRouter >
+    <HashRouter >
       <MainPage />    
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
