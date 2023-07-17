c9e0a5c0fb6e588a2604a178d545628c23780ca5
John Eldrin Tolentino <115896724+jeldrint@users.noreply.github.com>
2023-07-17 22:36:19 +0800

    Update 02_setScore.js

--- src/components/02_setScore.js
+++ src/components/02_setScore.js
@@ -1,5 +1,5 @@
 import {db} from '../firebase'
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, setDoc, collection, doc } from "firebase/firestore";
 
 const setScore = async (timer) =>{
     let name = prompt('Congratulations! You\'ve completed the game! Please enter your name.');
@@ -10,7 +10,7 @@ const setScore = async (timer) =>{
         mins: Math.floor(timer / 60),
         secs: timer % 60        
     });
-
+    await setDoc(doc(db, 'coordinates', 'time'),{time: 0})
 }
 
 export default setScore
